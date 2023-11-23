a4b5a040d9f4731fe2a341dcea07e693e417e737
TakaSoap <34995160+TakaSoap@users.noreply.github.com>
2023-11-24 03:18:07 +0800

    In progress...

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,293 @@
 <template>
     <div style="height: 100vh; position: relative">
         <n-layout position="absolute">
-            <n-layout-header class="header" bordered> PromptCraft </n-layout-header>
+            <n-layout-header class="header">
+                <div style="margin-left: auto">
+                    <n-space>
+                        <n-button size="small" quaternary @click="showAPIKeyModal">
+                            <template #icon>
+                                <n-icon>
+                                    <BuildOutline />
+                                </n-icon>
+                            </template>
+                            Set API Key
+                        </n-button>
+                        <n-button size="small" quaternary @click="themeStore.toggleTheme">
+                            <template #icon>
+                                <n-icon>
+                                    <Sunny v-if="themeStore.isDarkTheme" />
+                                    <Moon v-else />
+                                </n-icon>
+                            </template>
+                            Toggle Theme
+                        </n-button>
+                    </n-space>
+                </div>
+            </n-layout-header>
             <n-layout has-sider position="absolute" style="top: 4em; bottom: 4em">
                 <n-layout :native-scrollbar="false">
                     <div class="content">
-                        <n-h2>平山道</n-h2>
-                        <n-h2>平山道</n-h2>
-                        <n-h2>平山道</n-h2>
-                        <n-h2>平山道</n-h2>
-                        <n-h2>平山道</n-h2>
-                        <n-h2>平山道</n-h2>
+                        <n-gradient-text class="title" type="success">PromptCraft</n-gradient-text>
+                        <n-space class="main-body" vertical>
+                            <n-form inline>
+                                <n-form-item label="Your Prompt">
+                                    <n-input
+                                        class="textarea"
+                                        v-model:value="userPromptInput"
+                                        type="textarea"
+                                        show-feedback="false"
+                                        :autosize="autoSize"
+                                        :validation-status="userInputStatus"
+                                        :feedback="userInputFeedback"
+                                    />
+                                </n-form-item>
+                                <n-form-item label="Improved Prompt">
+                                    <n-spin :show="isPromptLoading" description="Crafting your prompt...">
+                                        <n-input
+                                            class="textarea"
+                                            v-model:value="improvedPromptComputed"
+                                            type="textarea"
+                                            placeholder="Nothing Here"
+                                            show-feedback="false"
+                                            :autosize="autoSize"
+                                        />
+                                    </n-spin>
+                                </n-form-item>
+                            </n-form>
+
+                            <n-form inline>
+                                <n-form-item label="Response 1">
+                                    <n-spin :show="isFirstResLoading" description="Waiting Response...">
+                                        <n-input
+                                            class="textarea"
+                                            v-model:value="userPromptResponseComputed"
+                                            type="textarea"
+                                            placeholder="Nothing Here"
+                                            :autosize="autoSize"
+                                        />
+                                    </n-spin>
+                                </n-form-item>
+                                <n-form-item label="Response 2">
+                                    <n-spin :show="isSecondResLoading" description="Waiting Response...">
+                                        <n-input
+                                            class="textarea"
+                                            v-model:value="improvedPromptResponseComputed"
+                                            type="textarea"
+                                            placeholder="Nothing Here"
+                                            :autosize="autoSize"
+                                        />
+                                    </n-spin>
+                                </n-form-item>
+                            </n-form>
+
+                            <n-grid cols="10" style="margin-bottom: 2em" x-gap="18">
+                                <n-gi span="3" offset="2">
+                                    <n-button block secondary type="Error">
+                                        <template #icon>
+                                            <n-icon>
+                                                <TrashBinOutline />
+                                            </n-icon>
+                                        </template>
+                                        Clear All
+                                    </n-button>
+                                </n-gi>
+                                <n-gi span="3" offset="">
+                                    <n-button block secondary type="primary" @click="handleSubmit">
+                                        <template #icon>
+                                            <n-icon>
+                                                <BulbOutline />
+                                            </n-icon>
+                                        </template>
+                                        Craft & Compare
+                                    </n-button>
+                                </n-gi>
+                            </n-grid>
+
+                            <n-collapse-transition class="feedback" :show="showFeedback">
+                                <n-card bordered style="max-width: 858px">
+                                    <template #header> Feedback </template>
+                                    <div v-if="isPromptLoading">
+                                        <n-skeleton text :repeat="2" />
+                                        <n-skeleton text style="width: 60%" />
+                                    </div>
+                                    <div v-else>
+                                        {{ promptFeedback }}
+                                    </div>
+                                </n-card>
+                            </n-collapse-transition>
+                        </n-space>
                     </div>
                 </n-layout>
             </n-layout>
-            <n-layout-footer class="footer" bordered position="absolute">
+            <n-layout-footer class="footer" position="absolute">
                 <n-text depth="3"> Copyright © 2023 &nbsp|&nbsp Gao Zesen </n-text>
             </n-layout-footer>
         </n-layout>
     </div>
+
+    <n-modal v-model:show="isAPIKeyModalVisible" preset="card" title="Set OpenAI API Key" style="width: 500px">
+        <n-input v-model:value="apiKey" placeholder="Input your OpenAI API Key Here" />
+        <template #footer>
+            <n-space justify="end">
+                <n-button secondary type="primary" @click="isAPIKeyModalVisible = false"> Confirm </n-button>
+            </n-space>
+        </template>
+    </n-modal>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BulbOutline, Sunny, Moon, TrashBinOutline, BuildOutline } from '@vicons/ionicons5';
+import { OpenAI } from 'openai';
+import { useMessage } from 'naive-ui';
+
+const themeStore = useThemeStore();
+const message = useMessage();
+
+const userPromptInput = ref('');
+const userInputStatus = ref('');
+const userInputFeedback = ref('');
+const improvedPrompt = ref('');
+const improvedPromptComputed = computed(() => {
+    return improvedPrompt.value;
+});
+const userPromptResponse = ref('');
+const userPromptResponseComputed = computed(() => {
+    return userPromptResponse.value;
+});
+const improvedPromptResponse = ref('');
+const improvedPromptResponseComputed = computed(() => {
+    return improvedPromptResponse.value;
+});
+const promptFeedback = ref('');
+
+const autoSize = {
+    minRows: 4,
+    maxRows: 20
+};
+
+const showFeedback = ref(false);
+const isPromptLoading = ref(false);
+const isFirstResLoading = ref(false);
+const isSecondResLoading = ref(false);
+
+const isAPIKeyModalVisible = ref(false);
+const apiKey = ref('');
+function showAPIKeyModal() {
+    isAPIKeyModalVisible.value = true;
+}
+
+const openai = new OpenAI({
+    apiKey: apiKey.value,
+    dangerouslyAllowBrowser: true
+});
+
+const modelSystemPrompt = 'You are a helpful assistant.';
+const promptImprovingSystemPrompt = `You are a professional prompt engineer.
+Your task is to improve the input prompt, apply different prompt engineering techniques, return to the improved prompt and provide a feedback that explains what improvements you made and the reasons behind them in order to better enable the user to learn prompt engineering.
+The output is formatted into a JSON structure with "improvedPrompt" and "feedback" keys.
+{"improvedPrompt": "string","feedback": "string"}`;
+
+const promptImprovingUserPrompt = computed(() => {
+    return `${userPromptInput.value}
+---
+The user inputted prompt is above, now try to improve it and return in given JSON format.`;
+});
+
+const gpt3ModelName = 'gpt-3.5-turbo-1106';
+
+async function openaiRequest(systemPrompt: string, contentPrompt: string, isJsonMode: boolean = false, model: string = 'gpt-4-1106-preview') {
+    openai.apiKey = apiKey.value;
+
+    if (isJsonMode) {
+        try {
+            const completion = await openai.chat.completions.create({
+                messages: [
+                    { role: 'system', content: systemPrompt },
+                    { role: 'user', content: contentPrompt }
+                ],
+                model: 'gpt-4-1106-preview',
+                response_format: { type: 'json_object' }
+            });
+
+            console.log(completion.choices[0].message.content);
+            return completion.choices[0].message.content;
+        } catch (err) {
+            console.error(err);
+            message.error('OpenAI API Request Failed\n' + err);
+        }
+    } else {
+        try {
+            const completion = await openai.chat.completions.create({
+                messages: [
+                    { role: 'system', content: systemPrompt },
+                    { role: 'user', content: contentPrompt }
+                ],
+                model: model
+            });
+
+            console.log(completion.choices[0].message.content);
+            return completion.choices[0].message.content;
+        } catch (err) {
+            console.error(err);
+            message.error('OpenAI API Request Failed\n' + err);
+        }
+    }
+}
+
+function handleSubmit() {
+    if (userPromptInput.value === '') {
+        userInputStatus.value = 'error';
+        userInputFeedback.value = 'Please input your prompt first';
+        return;
+    }
+
+    userInputStatus.value = 'success';
+    showFeedback.value = true;
+    isPromptLoading.value = true;
+    isFirstResLoading.value = true;
+    isSecondResLoading.value = true;
+
+    openaiRequest(promptImprovingSystemPrompt, promptImprovingUserPrompt.value, true).then((response) => {
+        if (response) {
+            const responseObject = JSON.parse(response);
+            improvedPrompt.value = responseObject.improvedPrompt;
+            promptFeedback.value = responseObject.feedback;
+            userInputStatus.value = '';
+
+            openaiRequest(modelSystemPrompt, improvedPrompt.value, false).then((response) => {
+                if (response) {
+                    improvedPromptResponse.value = response;
+                } else {
+                    message.error('OpenAI API Request Failed');
+                }
+                isSecondResLoading.value = false;
+            });
+        } else {
+            message.error('OpenAI API Request Failed');
+            showFeedback.value = false;
+        }
+        isPromptLoading.value = false;
+    });
+
+    openaiRequest(modelSystemPrompt, userPromptInput.value, false, gpt3ModelName).then((response) => {
+        if (response) {
+            userPromptResponse.value = response;
+        } else {
+            message.error('OpenAI API Request Failed');
+            showFeedback.value = false;
+        }
+        isFirstResLoading.value = false;
+    });
+}
+</script>
 
 <style scoped>
+.title {
+    font-size: 4em;
+    font-weight: 700;
+    margin: 8rem 0 1em 0;
+    user-select: none;
+}
 .content {
     display: flex;
     justify-content: center;
@@ -34,7 +298,7 @@
 
 .header {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     height: 4em;
     padding: 1em;
@@ -46,5 +310,13 @@
     align-items: center;
     height: 4em;
     padding: 1em;
+}
+
+.textarea {
+    width: 30em;
+}
+
+.main-body {
+    margin-bottom: 8rem;
 }
 </style>
